9e5b2db57bf74d4b9efb9e3db3359dbf538c4181
shruthi093 <jayasruthi.l@gmail.com>
2022-05-04 19:38:42 +0530

    strings

---
+++ scripts/strings.js
@@ -0,0 +1,80 @@
+/*let str1="Hello Javascript!"
+let arr1=["Hello","Java"]
+arr1[0]="Namaste"
+console.log(arr1)
+console.log(str1[20])
+str1[0]="X"
+console.log(str1)
+console.log(str1.length)
+
+//finding index
+let pos1=str1.indexOf("Java",0)
+let pos2=str1.indexOf("Java",pos1+1)
+console.log(pos1,pos2)
+let str1="Hello Javascript Java!"
+console.log(str1.search("Python"))
+
+//slicing
+console.log(str1.slice(-3,2))//not supported
+console.log(str1.slice(3,2))//not supported
+console.log(str1.slice(-10,-1))// supported
+console.log(str1.slice(-1,-10))// not supported
+
+
+let str1="Hello Java"
+console.log(str1.substring(0,5))//supported
+console.log(str1.substring(5,0))//supported swap least valye as start and greater as end
+console.log(str1.substring(-10,-1))//not supported for -ve indexing
+let str1="Hello Java"
+console.log(str1.substr(0,7))//second arg : length of the string to be trimmed (deprecated)
+//replacing strings
+let str1="Hello Java Javascript"
+console.log(str1.replace(/java/ig,"Python"))
+console.log(str1.replace(/java/i,"Python"))
+//console.log(str1.replaceAll("Java","Python")) -> regexp flag wont work , case sensitive
+console.log(str1.replaceAll(" ",","))
+
+//concatenation
+let str1="Hello Java Javascript"
+let str2="Python"
+let no=123
+console.log(str1.concat(",",str2))
+console.log(str1.concat(" ",no))
+
+//splliting string to array
+let str1="Hello Java Javascript"
+let arr=str1.split(" ")
+
+
+//array methods to convert array to string
+console.log(arr.toString().replaceAll(","," "))
+console.log(arr.join(" "))
+
+
+//accessing the characters at a given index
+let str1="Hello Java Javascript"
+console.log(str1[0])
+console.log(str1[30])
+console.log(str1.charAt(0))
+console.log(str1.charAt(30))*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
